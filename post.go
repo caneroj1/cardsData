31b1fb944f770f601f951241456438c34a82f84e
Joseph Canero <jcanero@Josephs-MacBook-Pro.local>
2015-09-05 10:28:18 -0400

    added a quick function to post to the service

---
+++ post.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"os"
+)
+
+// PostToDB sends a post request via curl to my local
+// cardsAPI to POST a new card
+func PostToDB() {
+	if len(os.Args) < 3 {
+		fmt.Println("Incorrect number of arguments supplied. The first argument after post must be the required body of the card.")
+		os.Exit(1)
+	}
+
+	postURL := "http://localhost:9000/card"
+	fmt.Printf("POSTING to %s\n", postURL)
+	for _, arg := range os.Args[2:] {
+		fmt.Printf("\t- %s\n", arg)
+	}
+
+	var (
+		cardType, blanks string
+	)
+
+	body := os.Args[2]
+	if len(os.Args) > 3 {
+		cardType = os.Args[3]
+	}
+	if len(os.Args) > 4 {
+		blanks = os.Args[4]
+	}
+
+	var client http.Client
+	data := make(url.Values)
+	data["cardBody"] = []string{body}
+	data["cardType"] = []string{cardType}
+	data["cardBlanks"] = []string{blanks}
+	resp, err := client.PostForm(postURL, data)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	code := resp.StatusCode
+	fmt.Printf("Status: %d\n", code)
+	fmt.Println(readBody(resp.Body))
+
+	// if code == 200 {
+	// 	fmt.Println(readBody(resp.Body))
+	// } else {
+	// 	fmt.Println(readErrors(resp.Cookies()))
+	// }
+}
+
+func readBody(body io.ReadCloser) string {
+	bytes := make([]byte, 100)
+	var returnStr string
+	for {
+		n, err := body.Read(bytes)
+		if err != nil {
+			if err == io.EOF {
+				if n > 0 {
+					returnStr += string(bytes[:n])
+				}
+				return returnStr
+			}
+		}
+		returnStr += string(bytes[:n])
+	}
+}
+
+func readErrors(cookies []*http.Cookie) string {
+	fmt.Println("Errors:")
+	for _, cookie := range cookies {
+		if cookie.Name == "REVEL_ERRORS" {
+			str, err := url.QueryUnescape(cookie.String())
+			if err != nil {
+				fmt.Println(err)
+			} else {
+				return str
+			}
+		}
+	}
+
+	return ""
+}
--- readData.go
+++ readData.go
@@ -1,129 +1,134 @@
 package main
 
 import (
-  "fmt"
-  "os"
-  "bufio"
-  "strconv"
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
 )
 
 // Card struct that represents a cards against humanity card
 type Card struct {
-  CardBody  string
-  CardType  int64
-  Blanks    int64
-  Classic   bool
+	CardBody string
+	CardType int64
+	Blanks   int64
+	Classic  bool
 }
 
 func getScanner() *bufio.Scanner {
-  return bufio.NewScanner(os.Stdin)
+	return bufio.NewScanner(os.Stdin)
 }
 
 func getContinueInput(scanner *bufio.Scanner) bool {
-  ok := scanner.Scan()
-  if !ok {
-    return false
-  }
-  return scanner.Text() == "y"
+	ok := scanner.Scan()
+	if !ok {
+		return false
+	}
+	return scanner.Text() == "y"
 }
 
 func createCard(scanner *bufio.Scanner) Card {
-  var card Card
-  var err error
-  fmt.Println("Enter 0 for white card, 1 for black card. Press enter for 0.")
-  ok := scanner.Scan()
-  if !ok {
-    panic("There was a problem scanning for input.")
-  }
+	var card Card
+	var err error
+	fmt.Println("Enter 0 for white card, 1 for black card. Press enter for 0.")
+	ok := scanner.Scan()
+	if !ok {
+		panic("There was a problem scanning for input.")
+	}
 
-  inp := scanner.Text()
-  if inp == "" {
-    card.CardType = 0
-  } else {
-    card.CardType, err = strconv.ParseInt(inp, 0, 0)
-    if err != nil {
-      card.CardType = 0
-    }
-  }
+	inp := scanner.Text()
+	if inp == "" {
+		card.CardType = 0
+	} else {
+		card.CardType, err = strconv.ParseInt(inp, 0, 0)
+		if err != nil {
+			card.CardType = 0
+		}
+	}
 
-  fmt.Println("Enter card body.")
-  ok = scanner.Scan()
-  if !ok {
-    panic("There was a problem scanning for input.")
-  }
+	fmt.Println("Enter card body.")
+	ok = scanner.Scan()
+	if !ok {
+		panic("There was a problem scanning for input.")
+	}
 
-  card.CardBody = scanner.Text()
-  if card.CardType == 1 {
-    fmt.Println("How many blank spaces does the card have?")
-    ok = scanner.Scan()
-    if !ok {
-      panic("There was a problem scanning for input.")
-    }
+	card.CardBody = scanner.Text()
+	if card.CardType == 1 {
+		fmt.Println("How many blank spaces does the card have?")
+		ok = scanner.Scan()
+		if !ok {
+			panic("There was a problem scanning for input.")
+		}
 
-    card.Blanks, err = strconv.ParseInt(scanner.Text(), 0, 0)
-    if err != nil {
-      panic("There was a problem converting that to a number.")
-    }
-  } else {
-    card.Blanks = 0
-  }
+		card.Blanks, err = strconv.ParseInt(scanner.Text(), 0, 0)
+		if err != nil {
+			panic("There was a problem converting that to a number.")
+		}
+	} else {
+		card.Blanks = 0
+	}
 
-  fmt.Println(card)
-  return card
+	fmt.Println(card)
+	return card
 }
 
 func writeOutput(cards *[]Card, id int) {
-  fileFlags := os.O_APPEND | os.O_RDWR | os.O_CREATE
-  fmt.Println("Writing Output.")
-  f, err := os.OpenFile("cards_data.txt", fileFlags, os.ModeAppend)
-  defer f.Close()
-  if err != nil {
-    fmt.Println("Could not open the file. Printing cards to screen.")
-    fmt.Println(cards)
-    panic(err)
-  }
+	fileFlags := os.O_APPEND | os.O_RDWR | os.O_CREATE
+	fmt.Println("Writing Output.")
+	f, err := os.OpenFile("cards_data.txt", fileFlags, os.ModeAppend)
+	defer f.Close()
+	if err != nil {
+		fmt.Println("Could not open the file. Printing cards to screen.")
+		fmt.Println(cards)
+		panic(err)
+	}
 
-  for _, card := range *cards {
-    _, err := f.WriteString(fmt.Sprintf("%d,\"%s\",%d,%d\n", id, card.CardBody, card.CardType, card.Blanks))
-    if err != nil {
-      fmt.Println("Could not write to file. Printing cards to screen.")
-      fmt.Println(cards)
-      panic(err)
-    }
-    id++
-  }
+	for _, card := range *cards {
+		_, err := f.WriteString(fmt.Sprintf("%d,\"%s\",%d,%d\n", id, card.CardBody, card.CardType, card.Blanks))
+		if err != nil {
+			fmt.Println("Could not write to file. Printing cards to screen.")
+			fmt.Println(cards)
+			panic(err)
+		}
+		id++
+	}
 }
 
 func makeCards() {
-  var cards []Card
-  cont := true
-  inputScanner := getScanner()
+	var cards []Card
+	cont := true
+	inputScanner := getScanner()
 
-  defer func() {
-    recover()
-    id, err := strconv.ParseInt(os.Args[2], 0, 0)
-    if err != nil {
-      id = 0
-    }
-    writeOutput(&cards, int(id))
-  }()
+	defer func() {
+		recover()
+		id, err := strconv.ParseInt(os.Args[2], 0, 0)
+		if err != nil {
+			id = 0
+		}
+		writeOutput(&cards, int(id))
+	}()
 
-  for cont {
-    cards = append(cards, createCard(inputScanner))
-    fmt.Print("Continue? (y)")
-    cont = getContinueInput(inputScanner)
-  }
+	for cont {
+		cards = append(cards, createCard(inputScanner))
+		fmt.Print("Continue? (y)")
+		cont = getContinueInput(inputScanner)
+	}
 }
 
 func main() {
-  if len(os.Args) < 2 {
-    fmt.Println("Try running it with:\ncardsData input [id to start at]\ncardsData write [path to csv file]")
-    os.Exit(0)
-  }
+	if len(os.Args) < 2 {
+		fmt.Println("Try running it with:")
+		fmt.Println("cardsData input [id to start at]")
+		fmt.Println("cardsData write [path to csv file]")
+		fmt.Println("cardsData post 'body' [type] [blanks]")
+		os.Exit(0)
+	}
 
-  if os.Args[1] == "input" {
-    makeCards()
-  } else if os.Args[1] == "write" {
-    WriteToDB()
-  }
+	if os.Args[1] == "input" {
+		makeCards()
+	} else if os.Args[1] == "write" {
+		WriteToDB()
+	} else if os.Args[1] == "post" {
+		PostToDB()
+	}
 }
